a376199b01e76eaa2916ba44f612ceb7ab43592d
Nikolaj Dyring <nikodyring@gmail.com>
2018-04-04 13:09:04 +0200

    Coin functionality added. Gives the scoresystem a purpose.

---
+++ Unity/Project Pear/Assets/Scripts/Coin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private ScoreSystem sm;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    sm = FindObjectOfType<ScoreSystem>();
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            sm.Coin++;
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Unity/Project Pear/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    [SerializeField]
+    private int coin = 0;
+
+    public int Coin
+    {
+        get { return coin; }
+        set { coin = value; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
