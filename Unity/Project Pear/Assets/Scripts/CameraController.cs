b8978fd953a83e856d359bbd4abd574b7523bf49
Nikolaj Dyring <nikodyring@gmail.com>
2018-04-12 10:58:56 +0200

    Spike Implemented + Few optimizations.

--- Unity/Project Pear/Assets/Scripts/CameraController.cs
+++ Unity/Project Pear/Assets/Scripts/CameraController.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Da vores spiller skal være i fokus, er det vigtigt at vi kan vælge hvilket objekt der er vores spiller.
-    public GameObject Player;
+    // Player object
+    private GameObject _player;
 
-    // We need the cameras position, and an offset, so that we don't move the camera inside our player, but instead uses an offset.
+    // We need the cameras position, and an offset, so that we don't move the camera inside our player, but instead uses an offset
     // See more @ https://docs.unity3d.com/ScriptReference/Vector3.html
     private Vector3 _offset;
 
     // When the scene is played this method will be called.
     void Start()
     {
-        // Creating the offset by taking the distance between the camera and the player object.
-        _offset = transform.position - Player.transform.position;
+        // Finding the player
+        _player = GameObject.Find("Player");
+
+        // Creating the offset by taking the distance between the camera and the player object
+        _offset = transform.position - _player.transform.position;
     }
 
     // Runs every frame, if the camera is active.
     void LateUpdate()
     {
-        // Adds the offset to the camera, so that it follows but keeps its distance.
-        transform.position = Player.transform.position + _offset;
+        CamFollow();
+    }
+
+    void CamFollow()
+    {
+        if (_player)
+        {
+            // Adds the offset to the camera, so that it follows but keeps its distance.
+            transform.position = _player.transform.position + _offset;
+        }
     }
 }
---
+++ Unity/Project Pear/Assets/Spike.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    // Player object
+    private GameObject _player;
+
+    // Runs at the start of the scene
+    void Start()
+    {
+        _player = GameObject.Find("Player");
+    }
+
+    // When another object enters the collider.
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // If the colliding object has the tag "Player" destroy the player.
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Destroy(_player);
+        }
+    }
+
+}
