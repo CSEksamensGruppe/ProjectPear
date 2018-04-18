e28920e13fee96640fe9a9ae4f25fa7940d92bea
Nikolaj Dyring <nikodyring@gmail.com>
2018-04-18 11:09:34 +0200

    Textmesh Pro imported. + Main Menu mockup is done.

---
+++ Unity/Project Pear/Assets/Scripts/MenuSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSystem : MonoBehaviour {
+
+    void StartGameOnClick()
+    {
+        SceneManager.LoadScene("Level_01");
+    }
+
+
+}
